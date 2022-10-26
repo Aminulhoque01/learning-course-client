15e0b72ce22d10c5aa874eb01200e65aef071f6a
Aminulhoque1 <aminulkupa50@gmail.com>
2022-10-26 11:22:19 -0700

    ten commit

--- src/Pages/Category/Category.js
+++ src/Pages/Category/Category.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import course3 from '../../asstes/Image/course3.jpg'
+
+import { HiDocument } from "react-icons/hi";
+import jsPDF from 'jspdf';
 
 
 
 const Category = () => {
     const Course= useLoaderData();
-    // console.log(singleCourses);
+    
+   
+    const handlerPdf =()=>{
+        const doc= jsPDF('landscape', 'px','a4', 'false');
+        
+        doc.addImage(Course.img, 65,20,500,400);
+        
+       
 
+        doc.save('a.pdf')
+    }
+    
     return (
         <div className='bg-dark'>
             <div className='container pt-5'>
                 <Card style={{  }}>
-                    <Card.Body>
-                        <Card.Title className='text-center'>{Course.name}</Card.Title>
+                    <Card.Body><HiDocument onClick={handlerPdf}></HiDocument>
+                    
+                        <Card.Title className='text-center'>{Course.name} </Card.Title>
                         <Card.Img variant="top" src={Course.img} />
                         <Card.Subtitle className="text-muted pt-3"><h2>{Course.title}</h2></Card.Subtitle>
                         <Card.Text className='pt-3'>
---
+++ src/Pages/ReactPdf/ReactPdf.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const ReactPdf = () => {
+    return (
+        <div>
+          
+        </div>
+    );
+};
+
+export default ReactPdf;
