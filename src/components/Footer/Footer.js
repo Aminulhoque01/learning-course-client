abefcbb78c806ee4934fb5d325611fb6ef74c874
Aminulhoque1 <aminulkupa50@gmail.com>
2022-10-27 09:47:29 -0700

    all fineshed

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div style={{height:"20vh"}} className='bg-black pt-2'>
+            <div className='pt-5'>
+                <p className='text-secondary text-center'>copyRight @ 2022...</p>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
